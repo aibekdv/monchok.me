42884dfa33d0f20c4f5f08bef43ed0b84fcd1a25
aibekdv <aibekdv@gmail.com>
2021-07-14 21:03:59 +0600

    edited header and footer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
   // slider top
   let swiper = new Swiper(".mySwiper", {
     pagination: {
